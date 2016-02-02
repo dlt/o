aa0b786486472e765194327dbe98016b785d0d67
Dalto Curvelano Jr <daltojr@gmail.com>
2016-02-02 16:58:04 -0200

    As for me, all I know is that I know nothing.

--- lib/o/parser.rb
+++ lib/o/parser.rb
@@ -2,47 +2,57 @@ require 'parslet'
 require 'pry'
 
 module O
+  class ASTBuilder < Parslet::Transform
+    rule(integer: simple(:x))  { { integer: Integer(x) } }
+    rule(string:  simple(:x))  { { string:  String(x)  } }
+    rule(boolean: simple(:x))  { { boolean: x == "#t"  } }
+    rule(float:   simple(:x))  { { float:   Float(x)   } }
+    rule(symbol:  simple(:x))  { { symbol:  x.to_sym   } }
+  end
+
   class Parser < Parslet::Parser
     include Contracts::Core
     include Contracts::Builtin
 
-    class TreeBuilder < Parslet::Transform
-      rule(:integer => simple(:x)) { { integer: Integer(x) } }
-      rule(:string  => simple(:x)) { { string: String(x) } }
-    end
-
     root :expression
 
     rule(:expression) {
-      integer | string
+      boolean | string | symbol |  float | integer | funcall
     }
 
     rule(:body) {
       (expression | identifier | float | integer | string).repeat.as(:exp)
     }
 
+    rule(:funcall) {
+      (
+        str("(") >>
+        space? >>
+        symbol.as(:funcname) >>
+        space? >>
+        (expression >> space?).repeat(1).as(:args) >>
+        space? >> str(')')
+      ).as(:funcall)
+    }
+
     rule(:space) {
       match('\s').repeat(1)
     }
+
     rule(:space?) {
       space.maybe
     }
 
-    rule(:identifier) {
-      (match('[a-zA-Z=*]') >> match('[a-zA-Z=*_]').repeat).as(:identifier) >> space?
-    }
-
-    rule(:float) {
-      (
-        integer >> (
-          str('.') >> match('[0-9]').repeat(1) |
-          str('e') >> match('[0-9]').repeat(1)
-        ).as(:e)
-      ).as(:float) >> space?
+    rule(:symbol) {
+      (match('[a-zA-Z=*-]') >> match('[a-zA-Z=*_-]').repeat).as(:symbol) >> space?
     }
 
     rule(:integer) {
       ((str('+') | str('-')).maybe >> match("[0-9]").repeat(1)).as(:integer) >> space?
+    }
+
+    rule(:float) {
+      (match('[0-9]') >> (str('.') >> match('[0-9]').repeat(1))).as(:float)
     }
 
     rule(:string) {
@@ -52,9 +62,13 @@ module O
       ).repeat.as(:string) >> str('"') >> space?
     }
 
+    rule(:boolean) {
+      (str("#f") | str("#t")).as(:boolean)
+    }
+
     Contract String => Hash
     def parse string
-      TreeBuilder.new.apply super
+      ASTBuilder.new.apply super
     rescue => error
       puts error.cause.ascii_tree
       raise error
--- test/o/test_parser.rb
+++ test/o/test_parser.rb
@@ -13,4 +13,28 @@ describe O::Parser do
   it 'should parse a string' do
     @parser.parse('"a string"').must_equal  string: 'a string'
   end
+
+  it 'should parse a boolean' do
+    @parser.parse('#f').must_equal  boolean: false
+    @parser.parse('#t').must_equal  boolean: true
+  end
+
+  it 'should parse floats' do
+    @parser.parse('1.0').must_equal  float: 1.0
+    @parser.parse('0.0001').must_equal  float: 0.0001
+  end
+
+  it 'should parse identifiers' do
+    @parser.parse('lambda').must_equal  symbol: :lambda
+    @parser.parse('-def').must_equal  symbol: :"-def"
+    @parser.parse('another-symbol').must_equal  symbol: :"another-symbol"
+  end
+
+  it 'should parse funcalls' do
+    @parser.parse('(list   1   )').must_equal  funcall: { funcname: {symbol: :list}, args: [{ integer: 1}] }
+    @parser.parse('(list  "string")').must_equal  funcall: { funcname: {symbol: :list}, args: [{ string: "string" }] }
+    @parser.parse('(list 1 2 3)').must_equal  funcall: { funcname: {symbol: :list}, args: [{ integer: 1}, {integer: 2}, {integer: 3}] }
+    @parser.parse('(list  (list 1))').must_equal  funcall: { funcname: {symbol: :list}, args: [{funcall: { funcname: {symbol: :list}, args: [{ integer: 1}] } }] }
+    @parser.parse('(list  (list "string"))').must_equal  funcall: { funcname: {symbol: :list}, args: [{funcall: { funcname: {symbol: :list}, args: [{ string: "string" }] } }] }
+  end
 end
